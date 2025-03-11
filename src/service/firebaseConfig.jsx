22dc7254336b29e31b9ff3aac0ca998a118c9168
Sai Rajath <sairajath7@gmail.com>
2025-03-10 23:31:57 -0400

    Firebase secrets

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Use environment variables for Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA71MR3TIR2nxMZMD3hWL4Ps_Yqo7_Zrn8",
-  authDomain: "tripsync-c1625.firebaseapp.com",
-  projectId: "tripsync-c1625",
-  storageBucket: "tripsync-c1625.firebasestorage.app",
-  messagingSenderId: "309412149060",
-  appId: "1:309412149060:web:f3695775117583f6760911",
-  measurementId: "G-YXF9VRY7K5",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
