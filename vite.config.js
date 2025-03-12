a6f47546862e2f107f8cf69375f03c24a9455c05
Sai Rajath <sairajath7@gmail.com>
2025-03-12 12:40:00 -0400

    Title update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "dist", // Ensure the build output goes to 'dist'
+    outDir: "dist",
   },
   resolve: {
     alias: {
